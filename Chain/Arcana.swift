5531bb73ca90ca22273cae893158004d4af50853
jitae <jitaekim93@gmail.com>
2016-08-29 00:13:25 -0700

    Removed optionals in Arcana Struct

--- Chain/Arcana.swift
+++ Chain/Arcana.swift
@@ -9,34 +9,34 @@
 struct Arcana {
     
     var uid: String?
-    var name: String?
-    var rarity: String?
-    var group: String?  // Class, 직업
-    var affiliation: String?
-    var cost: String?
-    var weapon: String?
+    var name: String
+    var rarity: String
+    var group: String  // Class, 직업
+    var affiliation: String
+    var cost: String
+    var weapon: String
     
-    var kizunaName: String?
-    var kizunaCost: String?
-    var kizunaAbility: String?
+    var kizunaName: String
+    var kizunaCost: String
+    var kizunaAbility: String
     
-    var skillName1 : String?
-    var skillMana1 : String?
-    var skillDesc1 : String?
+    var skillName1 : String
+    var skillMana1 : String
+    var skillDesc1 : String
     
-    var skillName2 : String?
-    var skillMana2 : String?
-    var skillDesc2 : String?
+    var skillName2 : String
+    var skillMana2 : String
+    var skillDesc2 : String
     
     var skillName3 : String?
     var skillMana3 : String?
     var skillDesc3 : String?
     
-    var abilityName1: String?
-    var abilityDesc1: String?
+    var abilityName1: String
+    var abilityDesc1: String
     
-    var abilityName2: String?
-    var abilityDesc2: String?
+    var abilityName2: String
+    var abilityDesc2: String
     
     init?(n: String, r: String, g: String, a: String, c: String, w: String, kN: String, kC: String, kA: String, sN1: String, sM1: String, sD1: String, sN2: String, sM2: String, sD2: String, sN3: String, sM3: String, sD3: String, aN1: String, aD1: String, aN2: String, aD2: String) {
         name = n
