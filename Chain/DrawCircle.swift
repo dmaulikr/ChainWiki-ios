0b2e50a4f4b59b0caf17f5885e9fa731b0d178dd
jitae <jitaekim93@gmail.com>
2016-09-13 02:08:29 -0700

    implemented lovely sphere

--- Chain/DrawCircle.swift
+++ Chain/DrawCircle.swift
@@ -11,43 +11,38 @@ import UIKit
 class DrawCircle: UIView {
 
     override func layoutSubviews()
-    { layer.cornerRadius = bounds.size.width/2; }
+    {
+        layer.cornerRadius = bounds.size.width/2
+    }
 
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
-//    override func drawRect(rect: CGRect) {
-//    
-//        var path = UIBezierPath(ovalInRect: CGRectInset(CGRect(x: 0, y: 0, width: 30, height: 30), 2, 2))
-//        path.fill()
-//        UIColor.greenColor().setStroke()
-//        path.stroke()
-//        
-//        let clippingPath = path.copy() as! UIBezierPath
-//        clippingPath.addCurveToPoint(CGPoint(x:self.bounds.width/2, y:0), controlPoint1: CGPoint(x:self.bounds.width/2, y:self.bounds.height/2), controlPoint2: CGPoint(x:self.bounds.width/2, y:self.bounds.height/2))
-//        clippingPath.closePath()
-//        
-//        clippingPath.addClip()
-//        
-//        //2 - get the current context
-//        let context = UIGraphicsGetCurrentContext()
-//        let colors = [UIColor.greenColor().CGColor, UIColor.whiteColor().CGColor]
-//        
-//        //3 - set up the color space
-//        let colorSpace = CGColorSpaceCreateDeviceRGB()
-//        
-//        //4 - set up the color stops
-//        let colorLocations:[CGFloat] = [0.0, 1.0]
-//        
-//        //5 - create the gradient
-//        let gradient = CGGradientCreateWithColors(colorSpace,
-//                                                  colors,
-//                                                  colorLocations)
-//        
-//        //6 - draw the gradient
-//        var startPoint = CGPoint(x:self.bounds.width/2, y:self.bounds.height)
-//        var endPoint = CGPoint(x:self.bounds.width/2, y: 0)
-//        CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, CGGradientDrawingOptions.DrawsAfterEndLocation)
-//    }
+    override func drawRect(rect: CGRect) {
+    
+        let context = UIGraphicsGetCurrentContext()
+        let locations: [CGFloat] = [0.0, 1.0]
+        
+        let colors = [UIColor.whiteColor().CGColor,
+                      UIColor.blueColor().CGColor]
+        
+        let colorspace = CGColorSpaceCreateDeviceRGB()
+        
+        let gradient = CGGradientCreateWithColors(colorspace,
+                                                  colors, locations)
+        
+        var startPoint = CGPoint()
+        var endPoint = CGPoint()
+        startPoint.x = self.bounds.width/4
+        startPoint.y = self.bounds.height/4
+        endPoint.x = self.bounds.width/2
+        endPoint.y = self.bounds.height/2
+        let startRadius: CGFloat = 0
+        let endRadius: CGFloat = 30
+        
+        CGContextDrawRadialGradient (context, gradient, startPoint,
+                                     startRadius, endPoint, endRadius,
+                                     CGGradientDrawingOptions.DrawsBeforeStartLocation)
+    }
     
     
 
